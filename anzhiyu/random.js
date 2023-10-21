8e9460be6ae9bb3300c2a7915cfac0119c6612aa
01kaikai <103395277+01kaikai@users.noreply.github.com>
2023-10-21 15:29:01 +0000

    发布新文章 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/02/hello-world/","2023/08/23/hexo命令/","2023/07/13/vue传值/","2023/07/07/数组方法/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/21/Elemint-Ui/","2023/10/21/hello-world/","2023/08/23/hexo命令/","2023/07/13/vue传值/","2023/07/07/数组方法/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
